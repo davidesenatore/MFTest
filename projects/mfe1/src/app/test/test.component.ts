3adc62b2fe46b453047c99f6b11cefa76944ba3a
Davide Senatore <info@ingeniumsoft.com>
2021-02-04 08:44:28 +0100

    First commit

---
+++ projects/mfe1/src/app/test/test.component.ts
@@ -0,0 +1,17 @@
+import { ApplicationRef, Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  //constructor(http:HttpClient) { }
+  
+  constructor() { }   //comment this line and uncomment the line above to see the error
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/shell/src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component, ComponentFactoryResolver, Injector, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'shell';
+
+  @ViewChild('placeHolder', { read: ViewContainerRef, static: true })
+  viewContainer: ViewContainerRef;
+
+  constructor(
+    private injector: Injector,
+    private cfr: ComponentFactoryResolver) { }
+
+  async ngOnInit() {
+
+    //Loading component from remote
+    /*
+    It's necessary:
+      the remote name (mfe1) 
+      exposedModule name (./Component) 
+      the name of the component to create (AppComponent)
+      remote entry url
+
+      In the remote there must be these settings in webpack.config.js      
+    */
+
+    /*
+      Loading and creating first remote component
+    */
+    const component = await loadRemoteModule({
+      remoteEntry: 'http://localhost:3000/remoteEntry.js',
+      remoteName: 'mfe1',
+      exposedModule: './Component'
+    }).then(m => m["AppComponent"]);
+
+
+    //Create component factory
+    const factory = this.cfr.resolveComponentFactory(component);
+    //Crete component dynamically and host it in the placeholder
+    this.viewContainer.createComponent(factory, null, this.injector);
+
+    /*
+      Loading and creating second remote component
+      WARNING! If TestComponent expose in constructor signature a reference to HttpClient, the creation of component fail
+    */
+    const component2 = await loadRemoteModule({
+      remoteEntry: 'http://localhost:3000/remoteEntry.js',
+      remoteName: 'mfe1',
+      exposedModule: './Test'
+    }).then(m => m["TestComponent"]);
+
+    //Create component factory
+    const factory2 = this.cfr.resolveComponentFactory(component2);
+    //Crete component dynamically and host it in the placeholder
+    this.viewContainer.createComponent(factory2);
+  }
+
+}
+
